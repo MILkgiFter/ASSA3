7d9f379178ecefc11875f6a6469584a04aae6822
Kuspekov Diyar se2117 <103980773+MILkgiFter@users.noreply.github.com>
2022-05-11 02:07:26 +0600

    Add files via upload

--- ASSA3/BST.java
+++ ASSA3/BST.java
@@ -1,5 +1,39 @@
 package ASSA3;
 
-public class BST {
+public class BST <K extends Comparable<K> , V> {
+private static class MyNode <K extends Comparable<K> , V>{
+    K key;
+    V value;
+    MyNode <K,V>left,right;
+    MyNode(K key ,V value)
+    {
+        this.key=key;
+        this.value=value;
 
+    }
+    private MyNode<K,V> root;
+    private int length;
+
+    public void put(K key,V value)
+    {
+        MyNode<K,V> newNode =new MyNode<>(key, value);
+        root=put(root, newNode);
+    }
+    private MyNode<K,V>put (MyNode<K,V>current ,MyNode<K,V>node) {
+        if (current == null) {
+            return node;
+        }
+        int cmp = node.key.compareTo(current.key);
+        if (cmp>0)
+            current.right=put(current.right, node);
+        else if(cmp<0)
+            current.left=put(current.left, node);
+        else {
+
+        }
+        return current;
+    }
+
+
+}
 }
--- ASSA3/HashTable.java
+++ ASSA3/HashTable.java
@@ -2,5 +2,111 @@ package ASSA3;
 
 public class HashTable<K , V> {
     private class HashNode<K , V>
+    {
+        private K key;
+        private V value;
+        private HashNode<K , V> next;
+        private HashNode(K key,V value)
+        {
+            this.value=value;
+            this.key=key;
+        }
+
+        @Override
+        public String toString() {
+            return "HashNode{" +
+                    "key=" + key +
+                    ", value=" + value +
+                    '}';
+        }
+    }
+    private  HashNode<K , V>[] chainArray;
+    private int M=11;
+    private int size;
+    public HashTable() {
+        this.M =11;
+        this.chainArray = new HashNode[M];
+        this.size = 0;
+    }
+    public HashTable(int M) {
+        this.M =M;
+        this.chainArray = new HashNode[M];
+        this.size = 0;
+    }
+    private int hash(K key)
+    {
+        return (key.hashCode()&0x7fffffff% M);
+    }
+
+    public void put(Integer key, String value){
+        if(key == null || value == null){
+            throw new IllegalArgumentException("Key or Value is null !!!");
+        }
+        int bucketIndex = getBucketIndex(key);
+        HashNode head = chainArray[bucketIndex];
+        while(head != null){
+            if(head.key.equals(key)){
+                head.value = value;
+                return;
+            }
+            head = head.next;
+        }
+        size++;
+        head = chainArray[bucketIndex];
+        HashNode node = new HashNode(key, value);
+        node.next = head;
+        chainArray[bucketIndex] = node;
+    }
+
+    private int getBucketIndex(Integer key){
+        return key % M;
+    }
+
+    public String get(Integer key){
+        if(key == null){
+            throw new IllegalArgumentException("Key is null !!!");
+        }
+        int bucketIndex = getBucketIndex(key);
+        HashNode head = chainArray[bucketIndex];
+        while(head != null){
+            if(head.key.equals(key)){
+                return head.toString();
+            }
+            head = head.next;
+        }
+
+        return null;
+    }
+
+    public String remove(Integer key){
+        if(key == null){
+            throw new IllegalArgumentException("Key is null !!!");
+        }
+
+        int bucketIndex = getBucketIndex(key);
+        HashNode head = chainArray[bucketIndex];
+        HashNode previous = null;
+
+        while(head != null){
+            if(head.key.equals(key)){
+                break;
+            }
+            previous = head;
+            head = head.next;
+        }
+        if(head == null){
+            return null;
+        }
+        size--;
+        if(previous != null){
+            previous.next = head.next;
+        } else {
+            chainArray[bucketIndex] = head.next;
+        }
+
+        return head.toString();
+    }
+
+
 
 }
